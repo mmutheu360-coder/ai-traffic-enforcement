eb63d64f3da4ce9206a7a6435062d18c3a9a1e1c
mmutheu360-coder <mmutheu360@gmail.com>
2026-08-31 16:40:41 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,11 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    global: {
-      fetch: (url, options = {}) =>
-        fetch(url, { ...options, cache: 'no-store' }),
-    },
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
